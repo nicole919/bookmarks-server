bb550cda6780ee22547e466113da058f4e84a525
Nicole Fafard <nicole.fafard@gmail.com>
2019-11-25 12:42:58 -0700

    heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 
+console.log("DB BITCH: ", process.env.DATABASE_URL);
 module.exports = {
+  validateChecksums: false,
   migrationDirectory: "migrations",
   driver: "pg",
   connectionString:
     process.env.NODE_ENV === "test"
-      ? process.env.TEST_DB_URL
-      : process.env.DB_URL
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+  ssl: !!process.env.SSL
 };
